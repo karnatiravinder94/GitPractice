a79bb5e13662ea73576e7c728b3cc8e683bd7cbd
kr <karnatiravinder94@gmail.com>
2025-03-21 11:47:40 +0530

    testing

--- LocalToGitRemoteSync/src/main/java/com/ht/TestingLocalToGitSynch.java
+++ LocalToGitRemoteSync/src/main/java/com/ht/TestingLocalToGitSynch.java
@@ -2,7 +2,7 @@ package com.ht;
 
 //Adding comment to main class
 public class TestingLocalToGitSynch {
-
+	//master feature
 	public static void main(String[] args) {
 		System.out.println("From main");
 	}
